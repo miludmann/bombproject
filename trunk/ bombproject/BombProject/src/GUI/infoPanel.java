8da6be895581610fc9461311f8736569e63c517c
g.depoyant <g.depoyant@4911ddb2-891b-aba6-2487-2de7e1fb646a>
2011-01-06 18:44:17 +0000

    Improving the interface... (still)

--- trunk/ bombproject/BombProject/src/GUI/infoPanel.java
+++ trunk/ bombproject/BombProject/src/GUI/infoPanel.java
@@ -1,9 +1,142 @@
 package GUI;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
 public class infoPanel extends JPanel {
 	
+	private JLabel info1;
+	private JLabel info2;
 	
+	public infoPanel(){
+		
+		BoxLayout mainLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
+		FlowLayout defaultLayout = new FlowLayout(FlowLayout.CENTER, 0, 0);
+		this.setLayout(mainLayout);
 
+		setInfo1(new JLabel());
+		getInfo1().setLayout(defaultLayout);
+		add(info1);
+		
+		setInfo2(new JLabel());
+		getInfo2().setLayout(defaultLayout);
+		add(info2);
+		
+		getInfo1().setText("");
+		getInfo2().setText("");
+		
+		getInfo1().setAlignmentX(CENTER_ALIGNMENT);
+		getInfo2().setAlignmentX(CENTER_ALIGNMENT);
+	}
+
+	public void setInfo1(JLabel info) {
+		this.info1 = info;
+	}
+
+	public JLabel getInfo1() {
+		return info1;
+	}
+	
+	public void changeColor(Color c){
+		getInfo2().setForeground(c);
+	}
+
+	public void setInfo2(JLabel info2) {
+		this.info2 = info2;
+	}
+
+	public JLabel getInfo2() {
+		return info2;
+	}
+	
+	
+	public void changeInfos(boolean isFinished,
+							boolean isTerrorist,
+							boolean isPlanted,
+							boolean isDefused){
+		
+		String display = "";
+
+		if(!isFinished)
+		{
+			if(isTerrorist)
+			{
+				if(isPlanted)
+				{
+					display+="Bomb planted !";
+				}
+				else
+				{
+					display+="Plant the Bomb !";
+				}
+			}
+			else
+			{
+				if(isPlanted)
+				{
+					display+="Sector Clear !";
+				}
+				else
+				{
+					display+="Defuse the Bomb !";
+				}
+			}
+		}
+		else
+		{
+			if(isTerrorist)
+			{
+				if(isPlanted)
+				{
+					if(isDefused)
+					{
+						display+="You lost: Bomb Defused";
+						changeColor(Color.MAGENTA);
+					}
+					else
+					{
+						display+="You won: Target Destroyed";
+						changeColor(Color.BLUE);
+					}
+				}
+				else
+				{
+					display+="You lost: Target Safe";
+					changeColor(Color.MAGENTA);
+				}
+			}
+			else
+			{
+				if(isPlanted)
+				{
+					if(isDefused)
+					{
+						display+="You won: Bomb Defused";
+						changeColor(Color.BLUE);
+					}
+					else
+					{
+						display+="You lost: Target Destroyed";
+						changeColor(Color.MAGENTA);
+					}
+				}
+				else
+				{
+					display+="You won: Target Safe";
+					changeColor(Color.BLUE);
+				}
+			}
+		}
+			
+		getInfo2().setText(display);
+	}
+	
+	public void showSide(boolean isTerrorist){
+		getInfo1().setText((isTerrorist?"":"Counter ")+"Terrorist");
+	}
 }
--- trunk/ bombproject/BombProject/src/GUI/rightGUI.java
+++ trunk/ bombproject/BombProject/src/GUI/rightGUI.java
@@ -1,18 +1,23 @@
 package GUI;
 
+import java.awt.Color;
+
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class rightGUI extends JPanel {
 	
-	private JPanel timePanel;
 	private JPanel infoPanel;
+	private JPanel timePanel;
 	
 	public rightGUI(){
 		
 		BoxLayout mainLayout = new BoxLayout(this, BoxLayout.Y_AXIS);
 		this.setLayout(mainLayout);
+				
+		this.setInfoPanel(new infoPanel());
+		this.add(this.getInfoPanel());
 		
 		this.setTimePanel(new timePanel());
 		this.add(this.getTimePanel());
@@ -24,6 +29,14 @@ public class rightGUI extends JPanel {
 
 	public JPanel getTimePanel() {
 		return timePanel;
+	}
+
+	public void setInfoPanel(JPanel infoPanel) {
+		this.infoPanel = infoPanel;
+	}
+
+	public JPanel getInfoPanel() {
+		return infoPanel;
 	}
 
 }
